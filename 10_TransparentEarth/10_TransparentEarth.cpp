19cc63192c7cf8940f27eada35ef952c40f2ace6
MalekIsart <malek.isart@gmail.com>
2019-10-04 18:29:15 +0200

    transparency example wip.

--- 10_TransparentEarth/10_TransparentEarth.cpp
+++ 10_TransparentEarth/10_TransparentEarth.cpp
@@ -1,6 +1,8 @@
 
 // A definir en premier lieu
 #if defined(_WIN32)
+#define WIN32_MEAN_AND_LEAN		1
+#define WIN32_EXTRA_LEAN		1
 // a definir globalement pour etre pris en compte par volk.c
 //#define VK_USE_PLATFORM_WIN32_KHR
 
@@ -42,6 +44,7 @@
 #include <glm/glm/glm.hpp>
 #include <glm/glm/gtc/matrix_transform.hpp>
 
+#define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
 
 #ifdef _DEBUG
@@ -68,6 +71,25 @@ static std::vector<char> readFile(const std::string& filename) {
 	return buffer;
 }
 
+enum PixelFormat
+{
+	PIXFMT_RGBA8,
+	PIXFMT_SRGBA8,
+	PIXFMT_DUMMY_ASPECT_DEPTH,
+	PIXFMT_DEPTH32F = PIXFMT_DUMMY_ASPECT_DEPTH,
+	MAX
+};
+
+enum ImageUsageBits
+{
+	IMAGE_USAGE_TEXTURE = 1<<0,
+	IMAGE_USAGE_BITMAP = 1<<1,
+	IMAGE_USAGE_RENDERTARGET = 1<<2,
+	IMAGE_USAGE_RENDERPASS = 1<<6,
+	IMAGE_USAGE_STAGING = 1<<7
+};
+typedef uint32_t ImageUsage;
+
 
 struct RenderSurface
 {
@@ -75,6 +97,17 @@ struct RenderSurface
 	VkImage image;
 	VkImageView view;
 	VkFormat format;
+
+	bool CreateSurface(struct VulkanRenderContext& rendercontext, int width, int height, PixelFormat pixelformat, ImageUsage usage = IMAGE_USAGE_TEXTURE|IMAGE_USAGE_BITMAP);
+	void Destroy(struct VulkanRenderContext& rendercontext);
+};
+
+struct Texture : RenderSurface
+{
+	VkSampler sampler;
+
+	bool Load(struct VulkanRenderContext& rendercontext, const char* filepath, bool sRGB = true);
+	void Destroy(struct VulkanRenderContext& rendercontext);
 };
 
 struct VulkanDeviceContext
@@ -134,6 +167,8 @@ struct VulkanRenderContext
 {
 	static const int PENDING_FRAMES = 2;
 
+	VulkanDeviceContext* context;
+
 	uint32_t graphicsQueueIndex;
 	uint32_t presentQueueIndex;
 	VkQueue graphicsQueue;
@@ -164,6 +199,248 @@ struct Buffer
 	VkBufferUsageFlags usage;
 	VkMemoryPropertyFlags properties;
 };
+
+
+bool RenderSurface::CreateSurface(VulkanRenderContext& rendercontext, int width, int height, PixelFormat pixelformat, ImageUsage usage)
+{
+	VulkanDeviceContext& context = *rendercontext.context;
+
+	// todo: fonction pixelformat to format
+	// todo: stencil, RGBA float16/32 etc..
+	switch (pixelformat)
+	{
+	case PIXFMT_DEPTH32F: format = VK_FORMAT_D32_SFLOAT; break; // 32 bit signed float
+	case PIXFMT_SRGBA8: format = VK_FORMAT_R8G8B8A8_SRGB; break;
+	case PIXFMT_RGBA8:
+	default: format = VK_FORMAT_R8G8B8A8_UNORM; break;
+	}
+
+	// TODO: staging
+	VkImageUsageFlags usageFlags = 0;
+	if (usage & IMAGE_USAGE_TEXTURE) {
+		usageFlags |= VK_IMAGE_USAGE_SAMPLED_BIT;
+		if (usage & IMAGE_USAGE_BITMAP)
+			usageFlags |= VK_IMAGE_USAGE_TRANSFER_DST_BIT;
+	}
+	if (usage & IMAGE_USAGE_RENDERTARGET) {
+		usageFlags |= pixelformat < PIXFMT_DEPTH32F ? VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT : VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT;
+		if (usage & IMAGE_USAGE_RENDERPASS)
+			usageFlags |= VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT;
+	}
+
+	VkImageCreateInfo imageInfo = {};
+	imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+	imageInfo.imageType = VK_IMAGE_TYPE_2D;
+	imageInfo.flags = 0;
+	imageInfo.extent.width = width;
+	imageInfo.extent.height = height;
+	imageInfo.extent.depth = 1; // <- 3D
+	imageInfo.mipLevels = 1;
+	imageInfo.arrayLayers = 1;
+	imageInfo.format = format;
+	imageInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
+	imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+	imageInfo.usage = pixelformat < PIXFMT_DUMMY_ASPECT_DEPTH ? VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT : VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT;
+	// | VK_IMAGE_USAGE_TRANSIENT_ATTACHMENT_BIT;
+	imageInfo.samples = (VkSampleCountFlagBits)1;//VK_SAMPLE_COUNT_1_BIT;
+	imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+	if (vkCreateImage(context.device, &imageInfo, nullptr, &image) != VK_SUCCESS) {
+		std::cout << "error: failed to create render target image!" << std::endl;
+		return false;
+	}
+
+	{
+		VkMemoryRequirements memRequirements;
+		vkGetImageMemoryRequirements(context.device, image, &memRequirements);
+
+		VkMemoryAllocateInfo allocInfo = {};
+		allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+		allocInfo.allocationSize = memRequirements.size;
+		// LAZILY_ALLOCATED couple a USAGE_TRANSIENT est utile sur mobile pour indiquer
+		// que la zone memoire est volatile / temporaire et qu'elle peut etre utilisee
+		// par toute autre partie du rendering lorsque notre render pass ne dessine pas dedans
+		// sur PC cela ne semble pas supporte par tous les drivers
+		VkMemoryPropertyFlags properties = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;//LAZILY_ALLOCATED_BIT;
+		allocInfo.memoryTypeIndex = context.findMemoryType(memRequirements.memoryTypeBits, properties);
+		if (allocInfo.memoryTypeIndex == ~0) {
+			properties = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
+			allocInfo.memoryTypeIndex = context.findMemoryType(memRequirements.memoryTypeBits, properties);
+		}
+		if (vkAllocateMemory(context.device, &allocInfo, nullptr, &memory) != VK_SUCCESS) {
+			std::cout << "error: failed to allocate image memory!" << std::endl;
+			return false;
+		}
+		if (vkBindImageMemory(context.device, image, memory, 0) != VK_SUCCESS) {
+			std::cout << "error: failed to bind image memory!" << std::endl;
+			return false;
+		}
+
+		VkImageViewCreateInfo viewInfo = {};
+		viewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+		viewInfo.image = image;
+		viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+		viewInfo.format = format;
+		viewInfo.subresourceRange = rendercontext.mainSubRange;
+		// l'IMAGE_ASPECT doit correspondre au pixel format (todo: stencil)
+		viewInfo.subresourceRange.aspectMask = pixelformat < PIXFMT_DUMMY_ASPECT_DEPTH ? VK_IMAGE_ASPECT_COLOR_BIT : VK_IMAGE_ASPECT_DEPTH_BIT;
+		// todo: add swizzling here when required
+
+		VkImageView imageView;
+		if (vkCreateImageView(context.device, &viewInfo, nullptr, &view) != VK_SUCCESS) {
+			std::cout << "failed to create texture image view!" << std::endl;
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void RenderSurface::Destroy(VulkanRenderContext& rendercontext)
+{
+	VulkanDeviceContext& context = *rendercontext.context;
+	vkDestroyImageView(context.device, view, nullptr);
+	vkDestroyImage(context.device, image, nullptr);
+	vkFreeMemory(context.device, memory, nullptr);
+}
+
+bool Texture::Load(VulkanRenderContext& rendercontext, const char* filepath, bool sRGB)
+{
+	VulkanDeviceContext& context = *rendercontext.context;
+	// pour des raisons de simplicite on force en RGBA quel que ce soit le format d'origine
+	int w, h, c;
+	uint8_t* pixels = stbi_load(filepath, &w, &h, &c, STBI_rgb_alpha);
+	uint32_t imageSize = w*h * 4;
+	VkBuffer stagingBuffer;
+	VkDeviceMemory stagingBufferMemory;
+
+	//createBuffer(imageSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, , stagingBuffer, stagingBufferMemory);
+	VkBufferCreateInfo bufferInfo = {};
+	bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+	bufferInfo.size = imageSize;
+	bufferInfo.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
+	DEBUG_CHECK_VK(vkCreateBuffer(context.device, &bufferInfo, nullptr, &stagingBuffer));
+
+	VkMemoryRequirements memRequirements;
+	vkGetBufferMemoryRequirements(context.device, stagingBuffer, &memRequirements);
+	VkMemoryAllocateInfo allocInfo = {};
+	allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+	allocInfo.allocationSize = memRequirements.size;
+	allocInfo.memoryTypeIndex = context.findMemoryType(memRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+	DEBUG_CHECK_VK(vkAllocateMemory(context.device, &allocInfo, nullptr, &stagingBufferMemory));
+	vkBindBufferMemory(context.device, stagingBuffer, stagingBufferMemory, 0);
+
+	void* data;
+	vkMapMemory(context.device, stagingBufferMemory, 0, imageSize, 0, &data);
+	memcpy(data, pixels, static_cast<size_t>(imageSize));
+	vkUnmapMemory(context.device, stagingBufferMemory);
+	
+	stbi_image_free(pixels);
+
+	RenderSurface::CreateSurface(rendercontext, w, h, sRGB ? PIXFMT_SRGBA8 : PIXFMT_RGBA8);
+
+	VkSamplerCreateInfo samplerInfo = {};
+	samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+	samplerInfo.minFilter = VK_FILTER_LINEAR;
+	samplerInfo.magFilter = VK_FILTER_LINEAR;
+	samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+	samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+	samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+	samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+	samplerInfo.mipLodBias = 0.0f;
+	samplerInfo.minLod = 0.0f;
+	samplerInfo.maxLod = 0.0f;
+	DEBUG_CHECK_VK(vkCreateSampler(context.device, &samplerInfo, nullptr, &sampler));
+
+	VkCommandBufferAllocateInfo commandInfo = {};
+	commandInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+	commandInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+	commandInfo.commandPool = rendercontext.mainCommandPool;
+	commandInfo.commandBufferCount = 1;
+
+	VkCommandBuffer commandBuffer;
+	vkAllocateCommandBuffers(context.device, &commandInfo, &commandBuffer);
+
+	VkCommandBufferBeginInfo beginInfo = {};
+	beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+	beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+	vkBeginCommandBuffer(commandBuffer, &beginInfo);
+
+
+	VkImageMemoryBarrier barrier = {};
+	barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+	barrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+	barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+	barrier.image = image;
+	barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+	barrier.subresourceRange.baseMipLevel = 0;
+	barrier.subresourceRange.levelCount = 1;
+	barrier.subresourceRange.baseArrayLayer = 0;
+	barrier.subresourceRange.layerCount = 1;
+
+	VkPipelineStageFlags sourceStage;
+	VkPipelineStageFlags destinationStage;
+
+	barrier.oldLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+	barrier.newLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
+	barrier.srcAccessMask = 0;
+	barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+	sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+	destinationStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+
+	vkCmdPipelineBarrier(commandBuffer,
+		sourceStage, destinationStage,
+		0, 0, nullptr, 0, nullptr, 1, &barrier);
+
+	VkBufferImageCopy region = {};
+	region.bufferOffset = 0;
+	region.bufferRowLength = 0;
+	region.bufferImageHeight = 0;
+	region.imageSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+	region.imageSubresource.mipLevel = 0;
+	region.imageSubresource.baseArrayLayer = 0;
+	region.imageSubresource.layerCount = 1;
+	region.imageOffset = { 0, 0, 0 };
+	region.imageExtent = { (uint32_t)w, (uint32_t)h, 1 };
+	vkCmdCopyBufferToImage(commandBuffer, stagingBuffer, image, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 1, &region);
+
+	barrier.oldLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
+	barrier.newLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+	barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+	barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+	sourceStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+	destinationStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+
+	vkCmdPipelineBarrier( commandBuffer,
+		sourceStage, destinationStage,
+		0, 0, nullptr, 0, nullptr, 1, &barrier);
+
+	vkEndCommandBuffer(commandBuffer);
+
+	VkSubmitInfo submitInfo = {};
+	submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+	submitInfo.commandBufferCount = 1;
+	submitInfo.pCommandBuffers = &commandBuffer;
+
+	vkQueueSubmit(rendercontext.graphicsQueue, 1, &submitInfo, VK_NULL_HANDLE);
+	vkQueueWaitIdle(rendercontext.graphicsQueue);
+
+	vkFreeCommandBuffers(context.device, rendercontext.mainCommandPool, 1, &commandBuffer);
+
+	vkDestroyBuffer(context.device, stagingBuffer, nullptr);
+	vkFreeMemory(context.device, stagingBufferMemory, nullptr);
+
+
+	return true;
+}
+
+void Texture::Destroy(VulkanRenderContext& rendercontext)
+{
+	VulkanDeviceContext& context = *rendercontext.context;
+	vkDestroySampler(context.device, sampler, nullptr);
+	RenderSurface::Destroy(rendercontext);
+}
 
 // scene data
 
@@ -214,6 +491,8 @@ struct Scene
 {
 	SceneMatrices matrices;
 	std::vector<Mesh> meshes;
+
+	Texture textures[1];
 
 	// GPU scene
 	// Un DescriptorSet ne peut pas etre update ou utilise par un command buffer
@@ -464,6 +743,8 @@ bool VulkanGraphicsApplication::Initialize()
 	vkGetDeviceQueue(context.device, rendercontext.graphicsQueueIndex, 0, &rendercontext.graphicsQueue);
 	rendercontext.presentQueue = rendercontext.graphicsQueue;
 
+	rendercontext.context = &context;
+
 	// swap chain
 
 	// todo: enumerer (cf validation)
@@ -577,73 +858,7 @@ bool VulkanGraphicsApplication::Initialize()
 	// Par contre, dans le cas ou un effet a besoin d'acceder au depth buffer, 
 	// il faut specifier STORE_OP_STORE pour le champ storeOp du depth attachment
 	RenderSurface& depthBuffer = rendercontext.depthBuffer;
-	depthBuffer.format = VK_FORMAT_D32_SFLOAT; // 32 bit signed float
-	{
-		VkImageCreateInfo imageInfo = {};
-		imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
-		imageInfo.imageType = VK_IMAGE_TYPE_2D;
-		imageInfo.flags = 0;
-		imageInfo.extent.width = context.swapchainExtent.width;
-		imageInfo.extent.height = context.swapchainExtent.height;
-		imageInfo.extent.depth = 1; // <- 3D
-		imageInfo.mipLevels = 1;
-		imageInfo.arrayLayers = 1;
-		imageInfo.format = rendercontext.depthBuffer.format;
-		imageInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
-		imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-		imageInfo.usage = VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT;// | VK_IMAGE_USAGE_TRANSIENT_ATTACHMENT_BIT;
-		imageInfo.samples = (VkSampleCountFlagBits)1;//VK_SAMPLE_COUNT_1_BIT;
-		imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
-
-		if (vkCreateImage(context.device, &imageInfo, nullptr, &rendercontext.depthBuffer.image) != VK_SUCCESS) {
-			std::cout << "error: failed to create render target image!" << std::endl;
-			return false;
-		}
-
-		{
-			VkMemoryRequirements memRequirements;
-			vkGetImageMemoryRequirements(context.device, depthBuffer.image, &memRequirements);
-
-			VkMemoryAllocateInfo allocInfo = {};
-			allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
-			allocInfo.allocationSize = memRequirements.size;
-			// LAZILY_ALLOCATED couple a USAGE_TRANSIENT est utile sur mobile pour indiquer
-			// que la zone memoire est volatile / temporaire et qu'elle peut etre utilisee
-			// par toute autre partie du rendering lorsque notre render pass ne dessine pas dedans
-			// sur PC cela ne semble pas supporte par tous les drivers
-			VkMemoryPropertyFlags properties = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;//LAZILY_ALLOCATED_BIT;
-			allocInfo.memoryTypeIndex = context.findMemoryType(memRequirements.memoryTypeBits, properties);
-			if (allocInfo.memoryTypeIndex == ~0) {
-				properties = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
-				allocInfo.memoryTypeIndex = context.findMemoryType(memRequirements.memoryTypeBits, properties);
-			}
-			if (vkAllocateMemory(context.device, &allocInfo, nullptr, &depthBuffer.memory) != VK_SUCCESS) {
-				std::cout << "error: failed to allocate render target image memory!" << std::endl;
-				return false;
-			}
-			if (vkBindImageMemory(context.device, depthBuffer.image, depthBuffer.memory, 0) != VK_SUCCESS) {
-				std::cout << "error: failed to bind render target image memory!" << std::endl;
-				return false;
-			}
-
-			VkImageViewCreateInfo viewInfo = {};
-			viewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
-			viewInfo.image = depthBuffer.image;
-			viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
-			viewInfo.format = depthBuffer.format;
-			viewInfo.subresourceRange = rendercontext.mainSubRange;
-			// on cree un depth buffer, l'IMAGE_ASPECT doit correspondre
-			viewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT;
-			// todo: add swizzling here when required
-
-			VkImageView imageView;
-			if (vkCreateImageView(context.device, &viewInfo, nullptr, &depthBuffer.view) != VK_SUCCESS) {
-				std::cout << "failed to create texture image view!" << std::endl;
-				return VK_NULL_HANDLE;
-			} 
-		}
-	}
-
+	depthBuffer.CreateSurface(rendercontext, context.swapchainExtent.width, context.swapchainExtent.height, PIXFMT_DEPTH32F, IMAGE_USAGE_RENDERTARGET);
 
 	VkAttachmentDescription attachDesc[RenderTarget::MAX] = {};
 	int id = RenderTarget::SWAPCHAIN;
@@ -882,10 +1097,8 @@ bool VulkanGraphicsApplication::Initialize()
 	vkDestroyShaderModule(context.device, vertShaderModule, nullptr);
 
 	// Matrices world des instances
-	for (int y = -5; y <= 5; y++) {
-		for (int x = -5; x <= 5; x++) {
-			scene.matrices.world[(y + 5) * 11 + (x + 5)] = glm::translate(glm::mat4(1.f), glm::vec3((float)x, (float)y, 0.f));
-		}
+	for (int x = -5; x <= 5; x++) {
+		scene.matrices.world[(x + 5)] = glm::translate(glm::mat4(1.f), glm::vec3((float)x, 0.f, 0.f));
 	}
 
 	// par defaut la matrice lookAt de glm est main droite (repere OpenGL, +Z hors de l'ecran)
@@ -1025,6 +1238,10 @@ bool VulkanGraphicsApplication::Initialize()
 		DEBUG_CHECK_VK(vkFlushMappedMemoryRanges(context.device, 1, &mappedRange));
 	}
 
+
+	scene.textures[0].Load(rendercontext, "ocean_mask.png", false);
+
+
 	m_frame = 0;
 	m_currentFrame = 0;
 
@@ -1039,6 +1256,8 @@ bool VulkanGraphicsApplication::Shutdown()
 		return false;
 
 	vkDeviceWaitIdle(context.device);
+
+	scene.textures[0].Destroy(rendercontext);
 
 	// VBO / IBO
 	for (int i = 0; i < Mesh::BufferType::MAX; i++)
@@ -1218,16 +1437,14 @@ bool VulkanGraphicsApplication::Display()
 	pushData.index = 0;
 	pushData.metalness = 0.0f;
 	pushData.reflectance = 0.5f;
-	for (float y = 0.f; y < 1.1f; y += 0.1f) {
-		pushData.metalness = y;
-		for (float x = 0.f; x < 1.1f; x += 0.1f) {
-			pushData.roughness = x;
 
-			vkCmdPushConstants(commandBuffer, rendercontext.mainPipelineLayout, VK_SHADER_STAGE_VERTEX_BIT| VK_SHADER_STAGE_FRAGMENT_BIT, 0, sizeof(PushData), &pushData);
-			vkCmdDrawIndexed(commandBuffer, scene.meshes[0].indices.size(), 1, 0, 0, 0);
+	for (float x = 0.f; x < 1.1f; x += 0.1f) {
+		pushData.roughness = x;
 
-			pushData.index++;
-		}
+		vkCmdPushConstants(commandBuffer, rendercontext.mainPipelineLayout, VK_SHADER_STAGE_VERTEX_BIT| VK_SHADER_STAGE_FRAGMENT_BIT, 0, sizeof(PushData), &pushData);
+		vkCmdDrawIndexed(commandBuffer, scene.meshes[0].indices.size(), 1, 0, 0, 0);
+
+		pushData.index++;
 	}
 
 	vkCmdEndRenderPass(commandBuffer);
